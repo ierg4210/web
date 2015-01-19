b283de4ec8214e2ec1b40889358e5ac0341369ba
adon <adon@yahoo-inc.com>
2015-01-20 04:16:13 +0800

    added lecture 3

---
+++ lectures/slide_config.js
@@ -0,0 +1,29 @@
+var SLIDE_CONFIG = {
+  // Slide settings
+  settings: {
+    title: 'User Interface Programming',
+    subtitle: 'IERG4210 Lecture 3',
+    eventInfo: {
+      title: '<br/>IERG4210 Web Programming and Security, 2015 Spring. <br/>Offered by Dept. of Information Engineering, The Chinese University of Hong Kong',
+    },
+    useBuilds: true, // Default: true. False will turn off slide animation builds.
+    usePrettify: true, // Default: true
+    enableSlideAreas: true, // Default: true. False turns off the click areas on either slide of the slides.
+    enableTouch: true, // Default: true. If touch support should enabled. Note: the device must support touch.
+    //analytics: 'UA-XXXXXXXX-1', // TODO: Using this breaks GA for some reason (probably requirejs). Update your tracking code in template.html instead.
+    favIcon: 'images/google_developers_logo_tiny.png',
+    fonts: [
+      'Open Sans:regular,semibold,italic,italicsemibold',
+      'Source Code Pro'
+    ],
+    //theme: ['mytheme'], // Add your own custom themes or styles in /theme/css. Leave off the .css extension.
+  },
+
+  // Author information
+  presenters: [{
+    name: 'Dr. Adonis Fung <br/> Information Engineering, CUHK <br/> Paranoids, Yahoo!',
+    //company: '<br/> Information Engineering, CUHK <br/> Paranoids, Yahoo!',
+    email: 'phfung@ie.cuhk.edu.hk',
+  }]
+};
+
